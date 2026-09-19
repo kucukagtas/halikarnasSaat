122de007ec9b4408a08d8ad2ed25cb8b25c4e519
kucukagtas <muhammedflix06@gmail.com>
2026-09-19 15:08:31 +0300

    start of Halikarnas

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btnScrollTop = document.getElementById('btnScrollTop');
+
+  if (btnScrollTop) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 280) {
+        btnScrollTop.classList.add('show');
+      } else {
+        btnScrollTop.classList.remove('show');
+      }
+    });
+
+    btnScrollTop.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  }
+});
